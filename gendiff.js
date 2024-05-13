3e25e95a5cd5df58445b03d1bb16ae717c5af6f4
ThisisHappyEL <1379This_is_HappyEL@mail.ru>
2024-05-13 18:50:05 +0700

    1 и 2 шаги

--- gendiff.js
+++ gendiff.js
@@ -3,18 +3,8 @@
 import { program } from 'commander';
 
 program
-  .name('string-util')
-  .description('CLI to some JavaScript string utilities')
-  .version('0.8.0');
-
-program.command('split')
-  .description('Split a string into substrings and display as an array')
-  .argument('<string>', 'string to split')
-  .option('--first', 'display just the first substring')
-  .option('-s, --separator <char>', 'separator character', ',')
-  .action((str, options) => {
-    const limit = options.first ? 1 : undefined;
-    console.log(str.split(options.separator, limit));
-  });
-
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.8.0')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1>', '<filepath2>');
 program.parse();
